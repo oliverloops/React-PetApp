5371e11280a01fc5b1e925772b498bb390bbcdf9
Oliver ALR <oa.lopezrodriguez@ugto.mx>
2020-03-17 01:14:30 -0600

    Error 404 added

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const path = require('path');
-// const cors = require('cors');
-const app = express();
-let port = process.env.PORT || 3000;
+const express = require('express'),
+      path = require('path'),
+      app = express();
+
+let port = process.env.PORT || 8080;
 
 // app.use(cors());
 app.use(express.static(path.join(__dirname, '/dist')));
+
+app.use((req, res) => {
+    res.status(404);
+    res.send("ERROR 404 - NOT FOUND");
+});
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'dist', 'page_3.html'));
